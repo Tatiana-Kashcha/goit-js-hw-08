349d49a33bd68a6351c6d61e8ffc9898d9e3cd13
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-05 14:46:25 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+// Шаблон розмітки галереї
+{
+  /* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+  </a>
+</li>; */
+}
+
+const galleryItemsEl = document.querySelector('.gallery');
+const makeGalleryItems = itemsEl => {
+  const { preview, original, description } = itemsEl;
+
+  return `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+const itemsImages = galleryItems.map(makeGalleryItems).join('');
+galleryItemsEl.insertAdjacentHTML('beforeend', itemsImages);
+
+const galleryLightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
