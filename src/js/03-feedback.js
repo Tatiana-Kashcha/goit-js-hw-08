137314175ad28b0059b189bcd745d5a87a3d97e8
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-07 14:39:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,12 +11,7 @@ const formData = {};
 formEl.addEventListener('submit', onSubmit);
 formEl.addEventListener('input', throttle(onFormInput, 500));
 
-// inputEmail.addEventListener('input', throttle(onEmailInput, 500));
-// textareaMessage.addEventListener('input', throttle(onTextareaInput, 500));
-
 notEmptyForm();
-// notEmptyEmail();
-// notEmptyTextarea();
 
 function onSubmit(event) {
   event.preventDefault();
@@ -41,15 +36,12 @@ function onFormInput(evt) {
   localStorage.setItem(STORAGE_KEY, formDataStorage);
 
   console.log(formData);
-  console.log(formDataStorage);
 }
 
 function notEmptyForm() {
   const savedValue = localStorage.getItem(STORAGE_KEY);
   if (savedValue) {
-    console.log(savedValue);
     const setValueForm = JSON.parse(savedValue);
-    console.log(setValueForm);
 
     if (setValueForm.email) {
       inputEmail.value = setValueForm.email;
@@ -62,37 +54,5 @@ function notEmptyForm() {
     } else {
       textareaMessage.value = '';
     }
-    console.log(setValueForm.email);
-    console.log(setValueForm.message);
   }
 }
-
-// function notEmptyTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-//   if (savedMessage) {
-//     console.log(savedMessage);
-//     textareaMessage.value = savedMessage;
-//     console.log(textareaMessage.value);
-//   }
-// }
-
-// function onTextareaInput(evt) {
-//   const messageText = evt.target.value;
-
-//   localStorage.setItem(STORAGE_KEY, messageText);
-// }
-
-// function onEmailInput(evt) {
-//   const inputText = evt.target.value;
-
-//   localStorage.setItem(STORAGE_KEY, inputText);
-// }
-
-// function notEmptyEmail() {
-//   const savedEmail = localStorage.getItem(STORAGE_KEY);
-//   if (savedEmail) {
-//     console.log(savedEmail);
-//     inputEmail.value = savedEmail;
-//     console.log(inputEmail.value);
-//   }
-// }
