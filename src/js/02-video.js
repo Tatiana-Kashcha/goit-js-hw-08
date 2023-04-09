5f368804882eb31afe096b2601d4dca0aa6b4db0
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-09 14:32:47 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,25 @@
 import throttle from 'lodash.throttle';
-import Vimeo, { Player } from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
+const playerSetItem = function (data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+};
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(playerSetItem, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
