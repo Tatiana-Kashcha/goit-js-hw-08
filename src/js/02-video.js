28622b12a715e0a453a38e8f60c124489deafced
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-09 20:10:59 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,30 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
+
 const playerSetItem = function (data) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
 };
 
+const secondsItem = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const playerGetItem = function (seconds) {
+  secondsItem;
+};
+
 player.on('timeupdate', throttle(playerSetItem, 1000));
+
+player
+  .setCurrentTime(secondsItem)
+  .then(playerGetItem)
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
 
 // player.on('play', function () {
 //   console.log('played the video!');
